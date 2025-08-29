8e93b8910464157c58b28124a74b8b7c320744f9
Geoffrey <gmezerette@gmail.com>
2025-08-29 20:21:12 +0200

    fix bootstrap/port-forward script

--- 00-bootstrap/argocd/install.sh
+++ 00-bootstrap/argocd/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "🚀 Bootstrap ArgoCD Homelab"
+echo "Bootstrap ArgoCD Homelab"
 
 #echo "Uninstall ArgoCD"
 #helm uninstall argocd -n argocd || echo "Release Not Found"
@@ -17,29 +17,26 @@ echo "🚀 Bootstrap ArgoCD Homelab"
 
 k3d cluster delete my-cluster
 
+echo "10 seconds ⏳"
 sleep 10
 
 k3d cluster create my-cluster --k3s-arg "--tls-san=192.168.1.98@server:*" -p "80:80@loadbalancer" -p "443:443@loadbalancer" --agents 1
 
-echo "📦 Mise à jour des dépendances Helm ArgoCD..."
-helm dependency update .
-echo "5 seconds ⏳"
-sleep 5
-
-echo "🚀 Installing ArgoCD (étape 1/2)..."
-
-# Installation ArgoCD uniquement
+echo "📦 Mise à jour des dépendances Helm ArgoCD & Installing ArgoCD (étape 1/2)..."
+helm dependency update . && \
 helm install argocd . --namespace argocd --create-namespace \
   -f values.yaml \
   -f ../../argocd-secret.yaml
 
+
 echo "⏳ Waiting for ArgoCD components to be ready..."
-kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=600s
-kubectl wait --for=condition=available deployment/argocd-repo-server -n argocd --timeout=600s
-kubectl wait --for=condition=available deployment/argocd-applicationset-controller -n argocd --timeout=600s
+kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
+kubectl wait --for=condition=available deployment/argocd-repo-server -n argocd --timeout=300s
+kubectl wait --for=condition=available deployment/argocd-applicationset-controller -n argocd --timeout=300s
 
 echo "⏳ Waiting for ArgoCD initialization..."
-sleep 45
+echo "30 seconds ⏳"
+sleep 30
 
 echo "🚀 Deploying App-of-Apps (étape 2/2)..."
 
@@ -50,11 +47,17 @@ echo "🔍 Checking configuration..."
 echo "=== Applications ArgoCD ==="
 kubectl get applications -n argocd
 
-echo "=== Repository Secrets ==="
+echo "=== Checking Repository Secrets ==="
 kubectl get secrets -n argocd -l argocd.argoproj.io/secret-type=repository
 
 echo "✅ Bootstrap complete!"
 echo "👤 Admin password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d 2>/dev/null || echo 'Secret not found')"
+
+echo " Waiting for ArgoCD to synchronize and then launch port-forward"
+echo "300 seconds ⏳"
+sleep 300
+
+k get applications -A
 
 # Démarrer le port-forward
 bash ../../port-forward.sh
